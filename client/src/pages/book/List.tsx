737df645f8cf63bff52ca45da731d761700eabef
manash-codes <manash.codes@gmail.com>
2024-11-15 11:51:48 +0530

    Update structure

--- client/src/pages/book/List.tsx
+++ client/src/pages/book/List.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Book } from "../../types/book"
 import { getBooks, removeBook } from "../../api/route"
 import { Link } from "react-router-dom"
+import Container from "../../components/Container"
 
 
 const ListPage = () => {
@@ -26,7 +27,7 @@ const ListPage = () => {
     }
 
     return (
-        <div className="px-4 py-2">
+        <Container>
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl text-start">Book List</h1>
                 <Link to="/add" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -36,7 +37,7 @@ const ListPage = () => {
 
             <ul className="my-4">
                 {books?.map((book) => (
-                    <li key={book._id} className="px-4 py-2 flex border my-2 rounded mb-2">
+                    <li key={book._id} className="px-4 py-2 flex border my-2 rounded mb-2 max-sm:flex-col gap-2">
                         <div className="flex-1">
                             <p className="text-lg">{book.title}</p>
                             <p className="text-gray-500">{book.author}</p>
@@ -48,7 +49,7 @@ const ListPage = () => {
                     </li>
                 ))}
             </ul>
-        </div>
+        </Container>
     )
 }
 
